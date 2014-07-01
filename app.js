ffb16bbdf9a5fe1b4fa0562a5b05c31ebd4890f4
Ashish Gaikwad <ash.gkwd@gmail.com>
2014-07-01 11:04:45 +0530

    States are working properly. Usable app is ready

--- app.js
+++ app.js
@@ -1,23 +1,33 @@
 angular.module('todoApp', ['ui.router'])
 .config(function($stateProvider, $urlRouterProvider) {
-	$urlRouterProvider.otherwise('/')
+	$urlRouterProvider
+		.when('/', '/all')
+		.otherwise('/all')
 
 	$stateProvider
 		.state('base', {
-			url: '/',
+			url: '',
+			abstract: true,
 			templateUrl: 'base.html',
 			controller: 'baseCtrl'
 		})
+		.state('base.all', {
+			url: '/all',
+			templateUrl: 'task-list.html',
+			controller: 'allCtrl'
+		})
 		.state('base.done', {
 			url: '/completed',
-			templateUrl: 'base.html'
+			templateUrl: 'task-list.html',
+			controller: 'doneCtrl'
 		})
 		.state('base.todo', {
 			url: '/todo',
-			templateUrl: 'base.html'
+			templateUrl: 'task-list.html',
+			controller: 'todoCtrl'
 		})
 })
-.controller('baseCtrl', function($scope, $state) {
+.controller('baseCtrl', function($scope) {
 	$scope.tasks = []
 
 	$scope.addTask = function() {
@@ -27,9 +37,22 @@ angular.module('todoApp', ['ui.router'])
 			"completed":false
 		})
 		$scope.newTask = ''
+		console.info($scope.tasks)
 	}
 
 	$scope.removeTask = function(task) {
 		$scope.tasks.splice($scope.tasks.indexOf(task), 1)
 	}
+})
+.controller('allCtrl', function($scope) {
+	console.log('doneCtrl is active')
+	$scope.stateFilter = null
+})
+.controller('doneCtrl', function($scope) {
+	console.log('doneCtrl is active')
+	$scope.stateFilter = {completed: true}
+})
+.controller('todoCtrl', function($scope) {
+	console.log('todoCtrl is active')
+	$scope.stateFilter = {completed:false}
 })
